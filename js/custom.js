bb4632edc4b69fabca53df8627a11a1e79f88a6d
ildikotuck <65176353+ildikotuck@users.noreply.github.com>
2020-06-13 23:16:41 +0930

    added faq

--- js/custom.js
+++ js/custom.js
@@ -8,5 +8,23 @@ document.querySelector('.burger').addEventListener('click', function() {
     document.querySelector('.burger').classList.toggle('open')
 
 
-} );
+});
     
+$('.question').click(function() {
+
+    $('.answer').slideUp();
+
+    if ( $(this).attr('data-open')) {
+
+        $(this).find('.answer').slideUp();
+        $(this).removeAttr('data-open');
+    
+    } else {
+        $('.answer').slideUp();
+        $('.question').removeAttr('data-open');
+        $(this).find('.answer').slideDown();
+        $(this).attr('data-open', true);
+    }
+
+
+});
